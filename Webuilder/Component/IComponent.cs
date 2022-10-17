50d26946de25a7756996d66d8594cb56d5b28465
Nikita Platonov <platonov1nv@gmail.com>
2022-10-17 21:11:59 +0300

    feat(Component): Add a component iterface
    Add an interface describing the basic functionality of a component.

---
+++ Webuilder/Component/IComponent.cs
@@ -0,0 +1,24 @@
+﻿/* An interface describing the basic functionality of a component
+ */
+
+namespace Webuilder.Component
+{
+    internal interface IComponent : IComponentContainer<IComponent>
+    {
+        string Name { get; }
+
+        string Class { get; }
+
+        string Id { get; }
+
+        int Width { get; }
+
+        int Grid { get; }
+
+        string Rander();
+
+        //IStyle[] Styles { get; }
+        
+        //IView View { get; }
+    }
+}
