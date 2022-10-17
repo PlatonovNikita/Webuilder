c54efc93453d07adc70c03742ea486167f098653
Nikita Platonov <platonov1nv@gmail.com>
2022-10-17 21:46:58 +0300

    dock(Component): Add a describe ComponentConteiner
    Add a describe the abstract class - ComponentConteiner, that implements the IComponentContainer interface

---
+++ Webuilder/Component/ComponentConteiner.cs
@@ -0,0 +1,27 @@
+﻿/* An abstract class that implements the IComponentContainer interface
+ */
+
+namespace Webuilder.Component
+{
+    internal abstract class ComponentConteiner : IComponentContainer<IComponent>
+    {
+        public virtual IComponent Parent => throw new NotImplementedException();
+
+        public virtual IComponent[] Components => throw new NotImplementedException();
+
+        public virtual void AddChild(IComponent component)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual IComponent GetComponent(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void RemoveChild(IComponent component)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
