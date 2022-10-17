c1dda6e6e8543c542596d01690cba4e4ba4486ba
Nikita Platonov <platonov1nv@gmail.com>
2022-10-17 21:53:21 +0300

    feat(Component.Helpers): Add ComponentErrorHelper
    Add a helper with component error

---
+++ Webuilder/Component/Helpers/ComponentErrorHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Webuilder.Helpers
+{
+    internal static class ComponentErrorHelper
+    {
+        const int LOVER_GRID_OR_WIDTH = 1;
+
+        const int UPPER_GRID = 12;
+
+        static public void CheckGrid(int grid)
+        {
+            CheckOutOfRange(grid, LOVER_GRID_OR_WIDTH, UPPER_GRID, 
+                            $"Value of grid component out of range ({LOVER_GRID_OR_WIDTH},{UPPER_GRID})!");
+        }
+
+        static public void CheckWidth(int width, int grid)
+        {
+            CheckOutOfRange(width, LOVER_GRID_OR_WIDTH, grid,
+                            $"Value of width component out of range ({LOVER_GRID_OR_WIDTH}, grid = {UPPER_GRID})!");
+        }
+
+        static void CheckOutOfRange(int value, int lower, int upper, string massage)
+        {
+            if (value < lower || value > upper)
+            {
+
+                throw new ArgumentOutOfRangeException(nameof(value), value, massage);
+            }
+        }
+    }
+}
