d9b2bdcee277d8bcb6c65ce254d3a6ad2dc96d4b
Nikita Platonov <platonov1nv@gmail.com>
2022-10-25 11:59:13 +0300

    feat(Component): Add implementation of Component
    Add SimpleComponent

---
+++ Webuilder/Component/Implementations/SimpleComponent/SimpleComponent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Webuilder.Component.Implementations.SimpleComponent
+{
+    public class SimpleComponent : Component
+    {
+        public static SimpleComponentBuilder GetBuilder()
+        {
+            return new SimpleComponentBuilder();
+        }
+
+        internal SimpleComponent(string name, string clas, string id, int width, int grid) 
+                          : base(name, clas, id, width, grid)
+        {
+        }
+
+        public override string Rander()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Webuilder/Component/Implementations/SimpleComponent/SimpleComponentBuilder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Webuilder.Helpers;
+
+namespace Webuilder.Component.Implementations.SimpleComponent
+{
+    public class SimpleComponentBuilder : ComponentBuilder<SimpleComponentBuilder>
+    {
+        private IComponent[]? _components;
+
+        public SimpleComponentBuilder WithComponents(IComponent[] components)
+        {
+            _components = components;
+            return this;
+        }
+
+        public SimpleComponent Build()
+        {
+            CheckDefauldComponentFields();
+
+            var simpleComponent = new SimpleComponent(_name, _clas, _localId, _width, _grid);
+
+            if (_components != null)
+            {
+                simpleComponent.AddCildren(_components);
+            }
+
+            return simpleComponent;
+        }
+    }
+}
