88b341ac1a2909655eed71eabb828f337932c27a
Nikita Platonov <platonov1nv@gmail.com>
2022-10-25 11:34:16 +0300

    feat(Component): Add implementation of Component
    Add ButtonComponent

---
+++ Webuilder/Component/Implementations/ButtonComponent/ButtonComponent.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Webuilder.Component.Implementations.ButtonComponent
+{
+    public class ButtonComponent : Component
+    {
+        private string _text;
+
+        private string _action;
+
+        private string[] _requiredParameters;
+
+        public string Text { get => _text;}
+
+        public string Action { get => _action;}
+
+        public string[]? RequiredParameters { get => _requiredParameters;}
+
+        public static ButtonComponentBuilder GetBuilder()
+        {
+            return new ButtonComponentBuilder();
+        }
+
+        internal ButtonComponent(string text, string action, string[] requiredParameters,
+                               string name, string clas, string id, int width, int grid) 
+                        : base(name, clas, id, width, grid)
+        {
+            _text = text;
+            _action = action;
+            _requiredParameters = requiredParameters;
+        }
+
+        public override string Rander()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Webuilder/Component/Implementations/ButtonComponent/ButtonComponentBuilder.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Webuilder.Helpers;
+
+namespace Webuilder.Component.Implementations.ButtonComponent
+{
+    public class ButtonComponentBuilder : ComponentBuilder<ButtonComponentBuilder>
+    {
+        private string? _text;
+
+        private string? _action;
+
+        private string[]? _requiredParameters;
+
+        public ButtonComponentBuilder WithText(string text)
+        {
+            ComponentErrorHelper.CheckForNull(text, nameof(text));
+            _text = text;
+            return this;
+        }
+
+        public ButtonComponentBuilder WithAction(string action)
+        {
+            ComponentErrorHelper.CheckForNull(action, nameof(action));
+            _action = action;
+            return this;
+        }
+
+        public ButtonComponentBuilder WithRequiredParameters(string[] requiredParameters)
+        {
+            ComponentErrorHelper.CheckForNull(requiredParameters, nameof(requiredParameters));
+            _requiredParameters = requiredParameters;
+            return this;
+        }
+
+        public ButtonComponent Build()
+        {
+            CheckDefauldComponentFields();
+
+            ComponentErrorHelper.CheckForNull(_text, nameof(_text));
+            ComponentErrorHelper.CheckForNull(_action, nameof(_action));
+
+            var buttonComponent = new ButtonComponent(_text, _action, _requiredParameters, 
+                                                      _name, _clas, _localId, _width, _grid);
+            return buttonComponent;
+        }
+    }
+}
